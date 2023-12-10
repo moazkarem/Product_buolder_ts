a16e6a8f3736a3a0165d1218595e7d0107ebadd7
Moaz Karem <MoazKarem@gmail.com>
2023-12-10 20:42:29 +0200

    add tailwind css to the project

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@
 const App = () => {
   return (
     <div>
-      app
+       <h1 className="text-3xl font-bold underline text-center text-red-600">
+      Hello world!
+    </h1>
     </div>
   )
 }
